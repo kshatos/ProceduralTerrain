f5418ecde84861ed780db73328a318a7c932276c
Keagan Shatos <kshatos@live.com>
2021-12-13 10:31:42 -0800

    Initial setup.

---
+++ ProceduralTerrain/main.cpp
@@ -0,0 +1,17 @@
+#include <Merlin/Core/core.hpp>
+
+using namespace Merlin;
+
+
+class ProceduralTerrainApp : public Application
+{
+public:
+    ProceduralTerrainApp() {}
+};
+
+
+void main()
+{
+    ProceduralTerrainApp app;
+    app.Run();
+}
