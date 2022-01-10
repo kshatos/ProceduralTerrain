0099421fe681d2ff6d90a0af556bd67530b3ea31
Keagan Shatos <kshatos@live.com>
2022-01-10 15:55:22 -0800

    Pulled new merlin commits.

--- ProceduralTerrain/main.cpp
+++ ProceduralTerrain/main.cpp
@@ -1,6 +1,8 @@
 #include <Merlin/Core/core.hpp>
 #include <Merlin/Scene/scene.hpp>
 #include <Merlin/Render/render.hpp>
+#include <backends/imgui_impl_opengl3.h>
+#include <imgui.h>
 #include <thread>
 #include "cube_sphere.hpp"
 #include "noise3d.hpp"
@@ -11,10 +13,12 @@ using namespace Merlin;
 class SceneLayer : public Layer
 {
     std::shared_ptr<Camera> camera;
+    std::shared_ptr<TransformComponent> camera_transform;
     std::shared_ptr<Texture2D> main_texture;
     std::shared_ptr<Material> main_material;
     std::shared_ptr<Cubemap> height_cubemap;
     std::shared_ptr<Cubemap> normal_cubemap;
+    std::shared_ptr<FrameBuffer> fbuffer;
     std::shared_ptr<Mesh<Vertex_XNTBUV>> mesh;
     GameScene scene;
 
@@ -123,28 +127,41 @@ public:
         //CalculateTangentFrame(mesh);
         auto varray = UploadMesh(mesh);
 
-        // Camera
-        camera = std::make_shared<PerspectiveCamera>(glm::pi<float>() / 3.0f, 1.0f, 0.1f, 20.0f);
-        camera->GetTransform().Translate(glm::vec3(0.0f, 0.0f, 5.0f));
-        scene.SetCamera(camera);
+        {// Camera
+            FrameBufferParameters fb_params;
+            fb_params.width = 1000;
+            fb_params.height = 1000;
+            fb_params.color_buffer_format = ColorBufferFormat::RGBA8;
+            fb_params.depth_buffer_format = DepthBufferFormat::DEPTH24_STENCIL8;
+            fbuffer = FrameBuffer::Create(fb_params);
 
+            camera = std::make_shared<PerspectiveCamera>(glm::pi<float>() / 3.0f, 1.0f, 0.1f, 20.0f);
+
+            auto entity = scene.CreateEntity();
+            auto camera_component = entity->AddComponent<CameraComponent>();
+            auto transform_comp = entity->AddComponent<TransformComponent>();
+            camera_component->data.camera = camera;
+            camera_component->data.frame_buffer = fbuffer;
+            camera_component->data.clear_color = glm::vec4(0.05f, 0.05f, 0.05f, 1.0f);
+            transform_comp->transform.Translate(glm::vec3(0.0f, 0.0f, 5.0f));
+
+            camera_transform = transform_comp;
+        }
         {// Sphere
-            auto entity = std::make_shared<Entity>();
+            auto entity = scene.CreateEntity();
             auto transform_comp = entity->AddComponent<TransformComponent>();
             auto mesh_render_comp = entity->AddComponent<MeshRenderComponent>();
-            mesh_render_comp->varray = varray;
-            mesh_render_comp->material = main_material;
-            scene.AddEntity(entity);
+            mesh_render_comp->data.vertex_array = varray;
+            mesh_render_comp->data.material = main_material;
         }
         {// Light
-            auto entity = std::make_shared<Entity>();
+            auto entity = scene.CreateEntity();
             auto light_comp = entity->AddComponent<DirectionalLightComponent>();
             light_comp->data.color = glm::vec3(0.5, 0.7, 0.5);
             light_comp->data.direction = glm::normalize(glm::vec3(0.0, -1.0, 0.0));
             light_comp->data.irradiance = 10.0f;
-            scene.AddEntity(entity);
         }
-
+        scene.OnAwake();
     }
 
     void OnDetatch() override
@@ -164,6 +181,23 @@ public:
             Renderer::Clear();
             scene.RenderScene();
         }
+        {
+            auto& io = ImGui::GetIO();
+            io.DisplaySize = ImVec2(
+                (float)Application::Get().GeMaintWindow()->GetWidth(),
+                (float)Application::Get().GeMaintWindow()->GetHeight());
+
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui::NewFrame();
+
+            auto s_buffer = fbuffer;
+            auto s_buffer_params = s_buffer->GetParameters();
+            uint32_t tex_id = s_buffer->GetColorAttachmentID();
+            ImGui::Image((ImTextureID)tex_id, ImVec2{ (float)s_buffer_params.width, (float)s_buffer_params.height });
+
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        }
     }
 
     virtual void HandleEvent(AppEvent& app_event) override
@@ -172,24 +206,24 @@ public:
 
     void MoveCamera(float time_step)
     {
-        const auto& up = camera->GetTransform().Up();
-        const auto& right = camera->GetTransform().Right();
-        const auto& forward = camera->GetTransform().Forward();
+        const auto& up = camera_transform->transform.Up();
+        const auto& right = camera_transform->transform.Right();
+        const auto& forward = camera_transform->transform.Forward();
         float speed = 5.0e-1f;
         if (Input::GetKeyDown(Key::W))
-            camera->GetTransform().Translate(+forward * speed * time_step);
+            camera_transform->transform.Translate(+forward * speed * time_step);
         if (Input::GetKeyDown(Key::A))
-            camera->GetTransform().Translate(-right * speed * time_step);
+            camera_transform->transform.Translate(-right * speed * time_step);
         if (Input::GetKeyDown(Key::S))
-            camera->GetTransform().Translate(-forward * speed * time_step);
+            camera_transform->transform.Translate(-forward * speed * time_step);
         if (Input::GetKeyDown(Key::D))
-            camera->GetTransform().Translate(+right * speed * time_step);
+            camera_transform->transform.Translate(+right * speed * time_step);
         if (Input::GetKeyDown(Key::Z))
-            camera->GetTransform().Translate(+up * speed * time_step);
+            camera_transform->transform.Translate(+up * speed * time_step);
         if (Input::GetKeyDown(Key::X))
-            camera->GetTransform().Translate(-up * speed * time_step);
+            camera_transform->transform.Translate(-up * speed * time_step);
 
-        camera->GetTransform().Rotate(up, Input::GetMouseScrollDelta().y * time_step * 20.0);
+        camera_transform->transform.Rotate(up, Input::GetMouseScrollDelta().y * time_step * 20.0);
     }
 };
 
