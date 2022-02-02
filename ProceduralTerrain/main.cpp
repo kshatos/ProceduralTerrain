c924f2eafebf614133b823c2316e21f474836c41
Keagan Shatos <kshatos@live.com>
2022-02-02 12:42:03 -0800

    Moved components to separate file. Added movement controller component to clean up main app code.

--- ProceduralTerrain/main.cpp
+++ ProceduralTerrain/main.cpp
@@ -8,33 +8,10 @@
 #include "cube_sphere.hpp"
 #include "noise3d.hpp"
 #include "erosion.hpp"
+#include "custom_components.hpp"
 
 
 using namespace Merlin;
-
-
-class RotatingPlanetComponent : public Component
-{
-    std::shared_ptr<TransformComponent> m_transform = nullptr;
-public:
-    glm::vec3 axis{ 0.0f, 1.0f, 0.0f };
-    float rotation_speed = 1.0f;
-
-    RotatingPlanetComponent(Entity* parent) : Component(parent)
-    {
-    }
-
-    void OnAwake() override
-    {
-        m_transform = m_parent->GetComponent<TransformComponent>();
-    }
-
-    void OnUpdate(float time_step)
-    {
-        m_transform->transform.Rotate(
-            axis, time_step * rotation_speed);
-    }
-};
 
 
 class SceneLayer : public Layer
@@ -346,6 +323,7 @@ public:
             auto entity = scene.CreateEntity();
             auto camera_component = entity->AddComponent<CameraComponent>();
             auto transform_comp = entity->AddComponent<TransformComponent>();
+            auto move_comp = entity->AddComponent<MovementControllerComponent>();
             camera_component->data.camera = camera;
             camera_component->data.frame_buffer = fbuffer;
             camera_component->data.clear_color = glm::vec4(0.05f, 0.05f, 0.05f, 1.0f);
@@ -381,7 +359,6 @@ public:
         time_elapsed += time_step;
         main_material->SetUniformFloat("time", time_elapsed);
 
-        MoveCamera(time_step);
         scene.OnUpdate(time_step);
         {
             Renderer::SetViewport(
@@ -438,27 +415,6 @@ public:
     {
     }
 
-    void MoveCamera(float time_step)
-    {
-        const auto& up = camera_transform->transform.Up();
-        const auto& right = camera_transform->transform.Right();
-        const auto& forward = camera_transform->transform.Forward();
-        float speed = 5.0e-1f;
-        if (Input::GetKeyDown(Key::W))
-            camera_transform->transform.Translate(+forward * speed * time_step);
-        if (Input::GetKeyDown(Key::A))
-            camera_transform->transform.Translate(-right * speed * time_step);
-        if (Input::GetKeyDown(Key::S))
-            camera_transform->transform.Translate(-forward * speed * time_step);
-        if (Input::GetKeyDown(Key::D))
-            camera_transform->transform.Translate(+right * speed * time_step);
-        if (Input::GetKeyDown(Key::Z))
-            camera_transform->transform.Translate(+up * speed * time_step);
-        if (Input::GetKeyDown(Key::X))
-            camera_transform->transform.Translate(-up * speed * time_step);
-
-        camera_transform->transform.Rotate(up, Input::GetMouseScrollDelta().y * time_step * 20.0);
-    }
 };
 
 class ProceduralTerrainApp : public Application
